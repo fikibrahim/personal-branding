7ca602eb5ddef07633c17223deb76a6b0ef0761d
fikibrahim <taufikibrahim21@gmail.com>
2026-09-17 12:42:23 +0700

    fix about section responsive layout

--- src/components/about/AboutHighlights.vue
+++ src/components/about/AboutHighlights.vue
@@ -27,7 +27,7 @@ const highlights = [
     <div class="mt-20 grid min-w-0 gap-6 md:grid-cols-2 lg:grid-cols-3" data-aos="fade-up">
         <BaseCard v-for="item in highlights" :key="item.title" class="group min-w-0">
             <div
-                class="mb-5 flex h-14 w-14 items-center justify-center rounded-2xl bg-cyan-500/10 text-3xl transition-all duration-300 group-hover:bg-cyan-500/20">
+                class="mb-5 flex h-14 w-14 shrink-0 items-center justify-center rounded-2xl bg-cyan-500/10 text-3xl transition-all duration-300 group-hover:bg-cyan-500/20">
                 {{ item.icon }}
             </div>
 
